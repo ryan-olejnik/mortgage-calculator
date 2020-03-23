40dced29e36e440549d2d03014a8b1badb403978
ryan-olejnik <ryan.olejnik@gmail.com>
2020-03-23 18:28:54 -0400

    Create form and calcualte monthly payment

--- App.js
+++ App.js
@@ -6,108 +6,73 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  TextInput,
+  View,
+  Alert
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Slider from '@react-native-community/slider';
 
 const App: () => React$Node = () => {
+  const [mortgageAmount, setMortgageAmount] = useState(100000);
+  const [interestRate, setInterestRate] = useState(3);
+  const [paybackYears, setPaybackYears] = useState(25);
+
+  const getMonthlyPayment = () => {
+    const i = interestRate / 100 / 12; // monthly interest rate
+    const p = mortgageAmount;
+    const n = paybackYears * 12; // Assuming MONTHLY
+
+    const monthlyPayment = p * ((i * Math.pow((1+i), n))/(Math.pow((1+i), n) - 1));
+    return monthlyPayment.toFixed(0);
+  };
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.root}>
+      <Text style={styles.header}>Mortgage Calculator</Text>
+      <TextInput
+        placeholder={'mortgage amount'}
+        style={{borderWidth: 1, borderColor: 'black', marginBottom: 25}}
+        onChangeText={value => setMortgageAmount(Number(value))}
+        value={String(mortgageAmount)}
+      />
+      <Text>Interest Rate - {interestRate.toFixed(2)} %</Text>
+      <Slider
+        step={0.01}
+        minimumValue={0}
+        maximumValue={6}
+        value={interestRate}
+        onValueChange={slideValue => setInterestRate(slideValue)}
+        minimumTrackTintColor="#1fb28a"
+        maximumTrackTintColor="#d3d3d3"
+        thumbTintColor="#b9e4c9"
+        style={{marginBottom: 20}}
+      />
+      <TextInput
+        placeholder={'Payback Years'}
+        style={{borderWidth: 1, borderColor: 'black', marginBottom: 25}}
+        onChangeText={value => setPaybackYears(Number(value))}
+        value={String(paybackYears)}
+      />
+      <Text style={{textAlign: 'center'}}>Monthly payments will be:</Text>
+      <Text style={{textAlign: 'center'}}>${getMonthlyPayment()}/m</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  root: {
+    // backgroundColor: '#8cacfa',
+    height: '100%',
+    padding: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  header: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 10,
   },
 });
 
